3792119c4d850b96d0d1aa13e5d8c6cc796fb389
최재은 <beckyi@douzone.com>
2021-04-19 20:09:10 +0900

    [mater] form, header 구성

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import Header from "./Header";
 import BottomBtn from "./BottomBtn";
 
@@ -13,23 +15,50 @@ const MainBox = styled.div`
   width: 414px;
   height: 100%;
   margin: 0px auto;
-  background: white;
+  background: #ffffff;
   position: relative;
 `;
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "100%"
+  },
+  header: {
+    flexGrow: 0
+  },
+  content: {
+    flexGrow: 1
+  }
+}));
+
 const MainForm = (props) => {
   const {noHead, noBtn, children} = props;
+  const classes = useStyles();
 
+console.log(children, props);
   return (
     <BackGround>
       <MainBox>
-        {!noHead &&
-          <Header />
-        }
-        {children}
-        {!noBtn &&
-          <BottomBtn />
-        }
+        <Grid
+          container
+          className={classes.container}
+          direction="column"
+          justify="space-between"
+          alignItems="stretch"
+          spacing={0}
+        >
+          {!noHead &&
+            <Grid item className={classes.header}>
+              <Header />
+            </Grid>
+          }
+          <Grid item className={classes.content}>
+            {children}
+          </Grid>
+          {!noBtn &&
+            <BottomBtn />
+          }
+        </Grid>
       </MainBox>
     </BackGround>
   );
