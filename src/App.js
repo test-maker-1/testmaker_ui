e5f642c913bc26280708968948f28ac955d509d0
최재은 <beckyi@douzone.com>
2021-04-20 21:26:13 +0900

    [refactor] frame 컴포넌트 withrouter로 분리 (헤더/버튼 영역 따로 관리)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
-
-import Header from "./components/Header";
-import BottomBtn from "./components/BottomBtn";
+import MainForm from "./components/frame/MainForm"
 import Feed from "./view/Feed";
 import PickTest from "./view/PickTest";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <BackGround>
-        <MainBox>
-          <Header />
-          <Main>
-            <Switch>
-              {/* view component */}
-              <Route exact path="/" component={Feed} />
-              <Route exact path="/pick-test" component={PickTest} />
-            </Switch>
-          </Main>
-          <BottomBtn />
-        </MainBox>
-      </BackGround>
+     <MainForm>
+      <Switch>
+        {/* view component */}
+        <Route exact path="/" component={Feed} />
+        <Route exact path="/pick-test" component={PickTest} />
+      </Switch>
+     </MainForm>
     </BrowserRouter>
   );
 };
-
-const BackGround = styled.div`
-  min-height: 100%;
-  background: #e6e6e6;
-`;
-
-const MainBox = styled.div`
-  position: relative;
-  margin: 0px auto;
-  width: min(414px, 100%);
-  height: 100%;
-  background: #ffffff;
-`;
-
-const Main = styled.main`
-  padding: 0 20px 80px 20px;
-  min-height: calc(100vh - 140px);
-  display: flex;
-
-  /* view component */
-  & > div {
-    flex: 1;
-  }
-`;
 
 export default App;
