18c7557431542505ed37822b5798c342fd85fc90
1000peach <_970813@daum.net>
2021-04-21 00:13:35 +0900

    [feat] ThemeProvider 적용 (styles/theme)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import './styles/reset.css'
-import App from './App'
-import store from './redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/reset.css";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+
+import App from "./App";
+import store from "./redux/store";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 
 // reportWebVitals();
---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const colors = {};
+
+const fontSizes = {};
+
+const paddings = {
+  main: 20
+};
+
+const widths = {
+  main: 414
+};
+
+const heights = {
+  header: 56,
+  bottomBtn: 80
+};
+
+const theme = { colors, fontSizes, widths, heights, paddings };
+
+export default theme;
