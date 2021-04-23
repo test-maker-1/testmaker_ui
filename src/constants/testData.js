8b41d3575375f944f1598318d7d0f5a237603955
1000peach <_970813@daum.net>
2021-04-24 00:45:04 +0900

    [feat] testData 추가

---
+++ src/constants/testData.js
@@ -0,0 +1,28 @@
+export const multipleQuestions = [
+  {
+    questionId: 1,
+    content: "내가 제일 좋아하는 여행지는?",
+    anwser: {
+      optionId: 1,
+      content: "보라카이"
+    },
+    options: [
+      {
+        optionId: 1,
+        content: "보라카이"
+      },
+      {
+        optionId: 2,
+        content: "제주도"
+      },
+      {
+        optionId: 3,
+        content: "스페인"
+      },
+      {
+        optionId: 4,
+        content: "발리"
+      }
+    ]
+  }
+];
