45e2393db4367dd375a3820c4213095f503033b5
1000peach <_970813@daum.net>
2021-04-24 21:36:12 +0900

    [feat] textarea font 스타일 추가

--- src/view/MultipleQA.jsx
+++ src/view/MultipleQA.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import { Option, AddOptionBtn } from "../components/common/Options";
 
 const MultipleQA = () => {
-  return <p>객관식 테스트 QA 입력 페이지</p>;
+  return (
+    <Container>
+      <Option questionId={1} value="보라카이" answer="제주도" />
+      <Option questionId={1} value="제주도" answer="제주도" />
+      <Option questionId={1} value="스페인" answer="제주도" />
+      <Option questionId={1} value="발리" answer="제주도" />
+      <AddOptionBtn />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default MultipleQA;
