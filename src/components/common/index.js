6821f0be775ba1727fbb47ace3b608b9eb3013ef
최재은 <beckyi@douzone.com>
2021-04-25 13:18:54 +0900

    [refact] 파일 추가 (테스트 스텝 1 추가, 위치 및 확장자 변경)

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,5 @@
 export {TitleBox} from "./TitleBox";
 export {BtnField, BtnShare} from "./BtnField";
 export {UploadImg} from "./UploadImg";
+export { Option, AddOptionBtn } from "./Options";
+export {default as TagInputBox} from "./TagInputBox";
--- src/constants/testStepComponents.js
+++ src/constants/testStepComponents.js
@@ -1,6 +1,8 @@
-import MultipleQA from "../view/MultipleQA";
+import MultipleStep1 from "../view/testmaking/multiple/MultipleStep1";
+import MultipleQA from "../view/testmaking/multiple/MultipleQA";
 
 export const multipleComponents = {
+  1: <MultipleStep1 />,
   2: <MultipleQA />
 };
 
