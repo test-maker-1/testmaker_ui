173a1e417a575058bb3c6e7aa742e1a45cb12df1
1000peach <_970813@daum.net>
2021-04-26 23:34:38 +0900

    [feat] createPromiseSaga(type, promiseCreator) 추가
    - 위치: src/utils/asyncUtils.js
    - 기능: action type과 promise function을 받아 요청, 요청 성공 or 요청 실패 action을 dispatch 해주는 saga

--- src/utils/asyncUtils.js
+++ src/utils/asyncUtils.js
@@ -1,5 +1,27 @@
+import { call, put } from "redux-saga/effects";
+
 export const createActionString = (type) => {
   return { success: `${type}Success`, error: `${type}Error` };
+};
+
+export const createPromiseSaga = (type, promiseCreator) => {
+  const { success, error } = createActionString(type);
+
+  return function* (action) {
+    try {
+      const response = yield call(promiseCreator, action.payload);
+      yield put({
+        type: success,
+        payload: response
+      });
+    } catch (err) {
+      yield put({
+        type: error,
+        payload: err.message,
+        error: true
+      });
+    }
+  };
 };
 
 export const reducerUtils = {
