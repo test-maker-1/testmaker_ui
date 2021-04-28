dae320dd7885b0cb8c348317cfd68bfb3c8ed695
1000peach <_970813@daum.net>
2021-04-28 17:40:52 +0900

    [refactor] store.js 사용하지 않는 코드 제거

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,17 +2,11 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./reducer";
 
-// import { composeWithDevTools } from 'redux-devtools-extension';
-
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: [...getDefaultMiddleware(), sagaMiddleware]
 });
-
-// _store.subscribe(); //변화 감지
 
 export default store;
