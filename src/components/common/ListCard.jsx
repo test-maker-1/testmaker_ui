719de5c1a9db0656a3af7c371f8165c018a95bbb
1000peach <_970813@daum.net>
2021-05-01 19:34:32 +0900

    [feat] common/ListCard 컴포넌트 추가

---
+++ src/components/common/ListCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import tempImg from "../../resources/temp-img.svg";
+
+const breakWidth = 350;
+const [pt, pl] = [20, 25];
+
+/*
+ * title: string;
+ * thumbnail: img file;
+ */
+const ListCard = ({ title, thumbnail = tempImg, children }) => {
+  return (
+    <Wrapper className="list-card">
+      <Thumbnail>
+        <img src={thumbnail} alt={title} />
+      </Thumbnail>
+      <div>
+        <Title>{title}</Title>
+        {children}
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin-bottom: 16px;
+  padding: ${pt}px ${pl}px;
+  display: flex;
+
+  border: 1px solid #e5e8ec;
+  border-radius: 5px;
+  background: #fafafa;
+
+  @media (max-width: ${breakWidth}px) {
+    flex-direction: column;
+  }
+`;
+
+const Thumbnail = styled.div`
+  padding-right: ${pl}px;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: ${breakWidth}px) {
+    padding-bottom: ${pt}px;
+  }
+`;
+
+const Title = styled.h2`
+  margin-bottom: 4px;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 27px;
+  color: #697382;
+`;
+
+export default ListCard;
