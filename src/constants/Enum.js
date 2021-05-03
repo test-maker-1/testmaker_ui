93f505275ade5a9543cf5f52b43d5e032073a282
1000peach <_970813@daum.net>
2021-05-03 21:31:31 +0900

    [feat] constants/Enum -> size 관련 상수 추가

--- src/constants/Enum.js
+++ src/constants/Enum.js
@@ -1,13 +1,16 @@
 // test type
 export const [multiple, mbti, weight] = ["multiple", "mbti", "weight"];
 
+// size
+export const [md, lg] = ["md", "lg"];
+
 const ENUM = {
   HOME: "home",
   PICKTEST: "picktest",
   PREVIEW: "preview",
   MOVENEXT: "movenext",
   SHARE: "share",
-  
+
   // svg type
   ADD: "add",
   CANCEL: "cancel",
