5719717ab1ac544485fed54accffe53b8a88c02e
1000peach <_970813@daum.net>
2021-05-04 19:06:48 +0900

    [feat] TestDetail에 테스트 설명 입력란 추가

--- src/view/testmaking/TestDetail.jsx
+++ src/view/testmaking/TestDetail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { TitleBox } from "../../components/common/index";
 import BottomBtn, { PageContainer } from "../../components/frame/BottomBtn";
-import { Input, InputTitle } from "../../styles/index";
+import { Input, InputTitle, TextArea } from "../../styles/index";
 
 import useMaking from "../../hooks/useMaking";
 import ENUM, { lg } from "../../constants/Enum";
@@ -11,7 +11,7 @@ const { PREVIEW, MOVENEXT } = ENUM;
 
 const TestDetail = () => {
   const { data, updateCommonByInput } = useMaking();
-  const { testName, optionalURL } = data;
+  const { testName, testDesc, optionalURL } = data;
 
   return (
     <PageContainer>
@@ -23,14 +23,22 @@ const TestDetail = () => {
           defaultValue={testName}
           onBlur={updateCommonByInput}
         />
-      </TitleBox>
-      <TitleBox title="나를 더 홍보할래요!" noline>
-        <Input
-          name="optionalURL"
-          placeholder="url 주소를 적어주세요"
-          defaultValue={optionalURL}
+        <TextArea
+          name="testDesc"
+          defaultValue={testDesc}
+          placeholder="테스트를 설명해주세요"
           onBlur={updateCommonByInput}
         />
+      </TitleBox>
+      <TitleBox title="나를 더 홍보할래요!" noline>
+        <form>
+          <Input
+            name="optionalURL"
+            placeholder="url 주소를 적어주세요"
+            defaultValue={optionalURL}
+            onBlur={updateCommonByInput}
+          />
+        </form>
       </TitleBox>
       <BottomBtn
         btnArr={[
