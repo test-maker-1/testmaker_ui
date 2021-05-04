335684252f9d33e7444e1107cb3e1d3c22647397
1000peach <_970813@daum.net>
2021-05-04 19:43:05 +0900

    [feat] common/Tag prettier 적용

--- src/components/common/Tag.jsx
+++ src/components/common/Tag.jsx
@@ -2,30 +2,30 @@ import React, { useCallback } from "react";
 import styled from "styled-components";
 
 const Tag = ({ tag, onDelete }) => {
-    const onClick = useCallback(() => {
-        onDelete(tag);
-    }, []);
-    return (
-        <TagBox>
-            {tag}
-            <button onClick={onClick}>❌</button>
-        </TagBox>
-    );
+  const onClick = useCallback(() => {
+    onDelete(tag);
+  }, []);
+  return (
+    <TagBox>
+      {tag}
+      <button onClick={onClick}>❌</button>
+    </TagBox>
+  );
 };
 
 export default Tag;
 Tag.propTypes = {};
 
 const TagBox = styled.div`
-    display: flex;
-    height: 31px;
-    width: max-content;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    font-size: 13px;
-    border-radius: 5px;
-    padding: 6px 10px;
-    border: 1px solid black;
-    margin-right: 10px;
+  display: flex;
+  height: 31px;
+  width: max-content;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+  border-radius: 5px;
+  padding: 6px 10px;
+  border: 1px solid black;
+  margin-right: 10px;
 `;
