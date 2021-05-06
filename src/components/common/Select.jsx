985905b32da7bf5dd9d254176d96cda5c9b32dc3
1000peach <_970813@daum.net>
2021-05-06 18:25:24 +0900

    [feat] MultiplePreset에 Select 추가
    - Select 컴포넌트에 이벤트 추가

--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -4,10 +4,17 @@ import styled from "styled-components";
 import { inputStyles } from "../../styles/index";
 import arrow from "../../resources/arrow.svg";
 
-const Select = ({ defaultValue, children }) => {
+/*
+ * name: string;
+ * value: string || number;
+ * onChange: function;
+ */
+const Select = ({ name, value, onChange, children }) => {
   return (
     <Wrapper>
-      <select defaultValue={defaultValue}>{children}</select>
+      <select name={name} value={value} onChange={onChange}>
+        {children}
+      </select>
     </Wrapper>
   );
 };
@@ -20,16 +27,12 @@ const Wrapper = styled.div`
   background: url(${arrow}) no-repeat right 8px center;
 
   select {
-    cursor: pointer;
-    color: #697382;
-
     width: 100%;
     padding: 12px 35px 12px 12px;
-    border: 0;
-
-    letter-spacing: -0.5px;
     font-size: 15px;
+    letter-spacing: -0.5px;
     line-height: 22px;
+    color: #697382;
   }
 
   option {
