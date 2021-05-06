3d4e7d677cf8df31a61b481e836adfa079fb3fb6
1000peach <_970813@daum.net>
2021-05-06 16:27:49 +0900

    [refacotr] styles/InputText : input type="text" 관련 스타일 컴포넌트 관리

---
+++ src/styles/InputText.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const inputStyles = `
+  width: 100%;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  background-color: #fafafa;
+  color: #697382;
+
+  &::placeholder {
+    color: #b7bdcb;
+  }
+`;
+
+export const Input = styled.input`
+  ${inputStyles}
+  margin-bottom: 16px;
+  padding: 12px;
+  font-size: 15px;
+  line-height: 22px;
+`;
+
+export const TextArea = styled.textarea`
+  ${inputStyles}
+  padding: 16px;
+  height: 138px;
+  font-size: 16px;
+  line-height: 29px;
+  letter-spacing: -0.5px;
+`;
+
+export default Input;
