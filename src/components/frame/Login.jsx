cad76216331f9feaa7b23c2116102570f7bf33af
rktguswjd <010_cm@naver.com>
2021-05-08 13:28:17 +0900

    [feat] login, register 라우팅 및 컴포넌트 추가

---
+++ src/components/frame/Login.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { withRouter, useHistory } from "react-router-dom";
+import { loginComponents } from "../../constants/loginModuleComponents";
+
+const LoginFrame = ({
+    match: {
+        params: { module, step },
+    },
+}) => {
+    const history = useHistory();
+    let components = module && loginComponents;
+
+    if (!components.hasOwnProperty(module)) {
+        history.push("/error");
+        return null;
+    }
+
+    if (module === "find-pw") {
+        let findPwComponent = components[module];
+        console.log(findPwComponent);
+        if (step == undefined || !findPwComponent.hasOwnProperty(step)) {
+            history.push("/error");
+            return null;
+        }
+
+        return findPwComponent[step];
+    } else return components[module];
+};
+
+export default withRouter(LoginFrame);
