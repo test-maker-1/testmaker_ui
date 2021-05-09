bd2ff3314b882ac1f10bbbf35bedd3816deeaac1
1000peach <_970813@daum.net>
2021-05-10 03:00:06 +0900

    [feat] RandomGuide : 랜덤 질문 가이드 컴포넌트 추가

--- src/view/testmaking/multiple/MultipleQnA.jsx
+++ src/view/testmaking/multiple/MultipleQnA.jsx
@@ -4,22 +4,61 @@ import styled from "styled-components";
 import BottomBtn, { PageContainer } from "../../../components/frame/BottomBtn";
 import ENUM from "../../../constants/Enum";
 
-const { MOVENEXT, PREVIEW } = ENUM;
+import { SVG } from "../../../components/common";
+
+const { MOVENEXT, PREVIEW, CASINO } = ENUM;
+
+const svgStyles = {
+  width: 32,
+  height: 32,
+  fill: "#0B70FD",
+};
 
 const MultipleQA = () => {
   return (
-    <>
-      <PageContainer>
-        객관식 테스트 입력 QnA 페이지
-        <BottomBtn
-          btnArr={[
-            { name: "미리보기", type: PREVIEW },
-            { name: "다 적었어요", type: MOVENEXT },
-          ]}
-        />
-      </PageContainer>
-    </>
+    <PageContainer>
+      <RandomGuide>
+        <div>
+          <SVG type={CASINO} style={svgStyles} />
+        </div>
+        <GuideText className="guide">
+          <h2>질문 생각하는 게 힘드시다구요?</h2>
+          <p>주사위를 누르면 랜덤으로 제공해드려요.</p>
+        </GuideText>
+      </RandomGuide>
+      <BottomBtn
+        btnArr={[
+          { name: "미리보기", type: PREVIEW },
+          { name: "다 적었어요", type: MOVENEXT },
+        ]}
+      />
+    </PageContainer>
   );
 };
+
+const RandomGuide = styled.section`
+  padding: 18px ${({ theme: { paddings } }) => paddings.main}px;
+  display: flex;
+  background-color: #fafafa;
+`;
+
+const GuideText = styled.div`
+  margin-left: 12px;
+  align-items: center;
+
+  h2 {
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 30px;
+    letter-spacing: -1px;
+  }
+
+  p {
+    font-size: 15px;
+    line-height: 24px;
+    letter-spacing: -0.5px;
+    color: #515966;
+  }
+`;
 
 export default MultipleQA;
