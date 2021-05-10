70b408db8f94f70a08e734825ba0a8c56ca4321a
rktguswjd <010_cm@naver.com>
2021-05-10 23:41:55 +0900

    [feat] frame/Header -> titleBtn 추가 및 css 변경

--- src/components/frame/Header.js
+++ src/components/frame/Header.js
@@ -4,23 +4,29 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import LeftBtn from "../Header/LeftBtn";
 import RightBtn from "../Header/RightBtn";
+import TitleBtn from "../Header/titleBtn";
 
 const dynamicHB = "#ffffff"; //white or opacity
 const btnArea = 50;
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    width: "100%",
-    height: "100%",
-    background: dynamicHB
-  },
-  side: {
-    width: `${btnArea}px`
-  },
-  center: {
-    width: `calc(100% - ${btnArea * 2}px)`,
-    textAlign: "center"
-  }
+    container: {
+        width: "100%",
+        height: "100%",
+        background: dynamicHB,
+    },
+    leftSide: {
+        width: `${btnArea}px`,
+        textAlign: "left",
+    },
+    rightSide: {
+        width: `${btnArea}px`,
+        textAlign: "right",
+    },
+    center: {
+        width: `calc(100% - ${btnArea * 2}px)`,
+        textAlign: "center",
+    },
 }));
 
 /*
@@ -28,39 +34,38 @@ const useStyles = makeStyles((theme) => ({
  * onToggle: function;
  */
 const Header = ({ header, onToggle }) => {
-  const { leftType, rightType, title } = header;
-  const classes = useStyles();
+    const { leftType, rightType, title } = header;
+    const classes = useStyles();
 
-  return (
-    <HeadContainer>
-      <Grid
-        container
-        className={classes.container}
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Grid item className={classes.side}>
-          <LeftBtn type={leftType} />
-        </Grid>
-        <Grid item className={classes.center}>
-          <h1>{title}</h1>
-        </Grid>
-        <Grid item className={classes.side}>
-          <RightBtn type={rightType} onToggleMenu={onToggle} />
-        </Grid>
-      </Grid>
-    </HeadContainer>
-  );
+    return (
+        <HeadContainer>
+            <Grid
+                container
+                className={classes.container}
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item className={classes.leftSide}>
+                    <LeftBtn type={leftType} />
+                </Grid>
+                <Grid item className={classes.center}>
+                    <TitleBtn type={title.type} title={title.title} />
+                </Grid>
+                <Grid item className={classes.rightSide}>
+                    <RightBtn type={rightType} onToggleMenu={onToggle} />
+                </Grid>
+            </Grid>
+        </HeadContainer>
+    );
 };
 
 const HeadContainer = styled.header`
-  position: sticky;
-  top: 0;
-  padding: 0 20px;
-  height: ${({ theme: { heights } }) => heights.header}px;
-  border: 1px solid black;
-  z-index: 100;
+    position: sticky;
+    top: 0;
+    padding: 0 20px;
+    height: ${({ theme: { heights } }) => heights.header}px;
+    z-index: 100;
 `;
 
 export default Header;
