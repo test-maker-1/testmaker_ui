c71b2ee9477c945ab8e6c08221aea310fe8a4a75
rktguswjd <010_cm@naver.com>
2021-05-10 23:50:17 +0900

    [refactor] constants/headerInfo -> title 구조 변경

--- src/constants/headerInfo.js
+++ src/constants/headerInfo.js
@@ -1,22 +1,23 @@
-export const [BACK, SEARCH, LOGO, MENU, NOTHING] = [
-  "back",
-  "search",
-  "logo",
-  "menu",
-  "nothing"
+export const [BACK, SEARCH, LOGO, MENU, NOTHING, TITLE] = [
+    "back",
+    "search",
+    "logo",
+    "menu",
+    "nothing",
+    "title",
 ];
 
 export const initHeader = {
-  leftType: SEARCH,
-  rightType: MENU,
-  title: "TEST MAKER"
+    leftType: SEARCH,
+    rightType: MENU,
+    title: { type: LOGO, title: null },
 };
 
 const headerInfo = {
-  "/test/multiple": {
-    leftType: BACK,
-    title: "객관식테스트"
-  }
+    "/test/multiple": {
+        leftType: BACK,
+        title: { type: TITLE, title: "객관식테스트" },
+    },
 };
 
 export default headerInfo;
