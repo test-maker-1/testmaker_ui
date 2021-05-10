d25e2e5b944b0f1ed3fe5ad6235c0ce6d057d599
rktguswjd <010_cm@naver.com>
2021-05-10 23:40:09 +0900

    [feat] SVG 공통 컴포넌트 cursor 수정

--- src/components/common/SVG.jsx
+++ src/components/common/SVG.jsx
@@ -1,32 +1,44 @@
-import React, {useMemo, useCallback, memo} from "react";
-import styled from "styled-components"
-import {svgInfo} from "../../resources/svgInfo";
+import React, { useMemo, useCallback, memo } from "react";
+import styled from "styled-components";
+import { svgInfo } from "../../resources/svgInfo";
 
-const SVG = ({type, onClick, style={}}) => {
-  const {width, height, viewBox, fill, inContext} = useMemo(() => svgInfo[type], [type]);
-  const _width = useMemo(() => style.width || width, [style.width]);
-  const _height = useMemo(() => style.height || height, [style.height]);
-  const _fill = useMemo(() => style.fill || fill, [style.fill]);
-  const handleOnClick = useCallback((event) => {
-    if(onClick) onClick(type, event);
-  }, [type, onClick]);
+const SVG = ({ type, onClick, style = {} }) => {
+    const { width, height, viewBox, fill, inContext } = useMemo(
+        () => svgInfo[type],
+        [type]
+    );
+    const _width = useMemo(() => style.width || width, [style.width]);
+    const _height = useMemo(() => style.height || height, [style.height]);
+    const _fill = useMemo(() => style.fill || fill, [style.fill]);
+    const handleOnClick = useCallback(
+        (event) => {
+            if (onClick) onClick(type, event);
+        },
+        [type, onClick]
+    );
 
-  return(
-    <SvgItem width={_width} height={_height} fill={_fill} 
-          x={0} y={0} viewBox={viewBox}
-          onClick={onClick ? handleOnClick : null}
-          preserveAspectRatio={"xMaxYMin meet"}
-          xmlns="http://www.w3.org/2000/svg">
-      {inContext.map((context, idx)=>{
-        return Object.assign({}, context, {key: `${type}_${idx}`}); //key값을 주기 위해
-      })}
-    </SvgItem>
-  );
+    return (
+        <SvgItem
+            width={_width}
+            height={_height}
+            fill={_fill}
+            x={0}
+            y={0}
+            viewBox={viewBox}
+            onClick={onClick ? handleOnClick : null}
+            preserveAspectRatio={"xMaxYMin meet"}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            {inContext.map((context, idx) => {
+                return Object.assign({}, context, { key: `${type}_${idx}` }); //key값을 주기 위해
+            })}
+        </SvgItem>
+    );
 };
 
 const SvgItem = styled.svg`
-  display: inline-block;
-  cursor: ${(props) => props.onClick ? "pointer" : "default"};
+    display: inline-block;
+    cursor: ${(props) => (props.onClick ? "default" : "pointer")};
 `;
 
 export default memo(SVG);
