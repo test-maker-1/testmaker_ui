6a1ab7a5f71fdcb1b9f9c7a1cb3f2d78bf12e60d
rktguswjd <010_cm@naver.com>
2021-05-10 23:44:12 +0900

    [refactor] Header/LeftBtn -> 구조 변경 및 아이콘 추가

--- src/components/Header/LeftBtn.jsx
+++ src/components/Header/LeftBtn.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { SVG } from "../common";
+import ENUM from "../../constants/Enum";
+import usePage from "../../hooks/usePage";
 
 import { BACK, SEARCH, LOGO, NOTHING } from "../../constants/headerInfo";
 
@@ -7,26 +10,22 @@ import { BACK, SEARCH, LOGO, NOTHING } from "../../constants/headerInfo";
  * type: string;
  */
 const LeftBtn = ({ type = BACK }) => {
-  return <>{leftBtn[type]}</>;
-};
-
-const BackBtn = () => {
-  return <Button>뒤로</Button>;
-};
-
-const SearchBtn = () => {
-  return <Button>검색</Button>;
-};
-
-const LogoBtn = () => {
-  return <Button>로고</Button>;
+    const { goBack } = usePage();
+    const onClickEvent = () => {
+        switch (type) {
+            case BACK:
+                return goBack();
+            default:
+                return null;
+        }
+    };
+    return <Button onClick={onClickEvent}>{leftBtn[type]}</Button>;
 };
 
 const leftBtn = {
-  [BACK]: <BackBtn />,
-  [SEARCH]: <SearchBtn />,
-  [LOGO]: <LogoBtn />,
-  [NOTHING]: null
+    [BACK]: <SVG type={ENUM.BACK} style={{ width: "24", height: "24" }} />,
+    [SEARCH]: <SVG type={ENUM.SEARCH} style={{ width: "24", height: "24" }} />,
+    [NOTHING]: null,
 };
 
 const Button = styled.button``;
