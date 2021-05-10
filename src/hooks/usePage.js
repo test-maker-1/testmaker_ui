149747900961ff167eb66811e9ce452cde13f892
rktguswjd <010_cm@naver.com>
2021-05-10 19:11:33 +0900

    [feat] hooks/usePage -> replace 추가

--- src/hooks/usePage.js
+++ src/hooks/usePage.js
@@ -1,21 +1,29 @@
 import { useHistory } from "react-router";
 
 const usePage = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  // pathname: string;
-  const goPage = (pathname, search = null, state = null) => {
-    history.push({
-      pathname,
-      search,
-      state,
-    });
-  };
+    const replace = (pathname, search = null, state = null) => {
+        history.replace({
+            pathname,
+            search,
+            state,
+        });
+    };
 
-  const go = () => history.go(); // go front
-  const goBack = () => history.goBack(); // go back
+    // pathname: string;
+    const goPage = (pathname, search = null, state = null) => {
+        history.push({
+            pathname,
+            search,
+            state,
+        });
+    };
 
-  return { goPage, go, goBack };
+    const go = () => history.go(); // go front
+    const goBack = () => history.goBack(); // go back
+
+    return { goPage, go, goBack, replace };
 };
 
 export default usePage;
