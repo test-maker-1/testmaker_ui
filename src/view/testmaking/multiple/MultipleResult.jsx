376ecf8f2ab61791b4723da8a4adcdbd6cf5a9dc
1000peach <_970813@daum.net>
2021-05-17 02:53:48 +0900

    [feat] 점수모드 버튼 ui 추가

--- src/view/testmaking/multiple/MultipleResult.jsx
+++ src/view/testmaking/multiple/MultipleResult.jsx
@@ -1,15 +1,43 @@
-import React, { useEffect, useMemo } from "react";
+import React from "react";
 import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
 
-import { TitleBox } from "../../../components/common";
+import { TitleBox, Title, SVG } from "../../../components/common";
 import BottomBtn, { PageContainer } from "../../../components/frame/BottomBtn";
 import { BtnAdd, Result } from "../../../components/making";
+
 import { TextBox } from "../../../styles";
+import theme from "../../../styles/theme";
 
 import useMaking from "../../../hooks/useMaking";
 import ENUM from "../../../constants/Enum";
 
 const { PREVIEW, MOVENEXT } = ENUM;
+
+const useStyles = makeStyles(() => ({
+  btnRank: () => ({
+    padding: 0,
+    fontSize: `${theme.fontSizes.sm}rem`,
+    fontWeight: "bold",
+    letterSpacing: -0.5,
+    lineHeight: "24px",
+    color: theme.colors.blue,
+    
+    "& .MuiButton-startIcon": {
+      marginRight: "4px !important",
+    },
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  }),
+}));
+
+const svgStyle = {
+  width: 24,
+  height: 24,
+  fill: theme.colors.blue,
+};
 
 const MultipleResult = () => {
   const {
@@ -19,10 +47,20 @@ const MultipleResult = () => {
   } = useMaking();
 
   const { questionsCnt, totalPoints, results } = data;
+  const classes = useStyles();
 
   return (
     <PageContainer>
-      <TitleBox title="구간 별 결과" noline>
+      <TitleBox noline>
+        <TitleWrap>
+          <Title>구간 별 결과</Title>
+          <Button
+            className={classes.btnRank}
+            startIcon={<SVG style={svgStyle} type={ENUM.CHANGE} />}
+          >
+            점수 모드
+          </Button>
+        </TitleWrap>
         <TextBox>
           {/* summary */}
           <Summary type="questions" value={questionsCnt} />
@@ -65,6 +103,12 @@ const Summary = ({ type, value }) => {
     </div>
   );
 };
+
+const TitleWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
 
 const SummaryText = styled.div`
   text-align: center;
