96f51b9dc7adf93bf1fd1151cf7751932131ec49
rktguswjd <010_cm@naver.com>
2021-05-16 22:48:05 +0900

    [feat] ImageView 컴포넌트 스타일 변경 및 border props 추가

--- src/components/common/ImageView.jsx
+++ src/components/common/ImageView.jsx
@@ -4,21 +4,21 @@ import styled from "styled-components";
 import SVG from "./SVG";
 import Enum from "../../constants/Enum";
 
-const EmptyImg = ({ isNull }) => {
+const EmptyImg = ({ isNull, border }) => {
   return isNull ? null : (
-    <EmptyBox>
+    <EmptyBox border={border}>
       <SVG type={Enum.PICTURE} />
     </EmptyBox>
   );
 };
 
-const ImageView = memo(({ isNull, imageUrl, height }) => {
+const ImageView = memo(({ isNull, imageUrl, height, border = true }) => {
   return (
     <ContainerImg height={height} className="flex-box">
       {imageUrl ? (
-        <ImgArea src={imageUrl} alt={"ImgArea"} />
+        <ImgArea src={imageUrl} alt={"ImgArea"} border={border} />
       ) : (
-        <EmptyImg isNull={isNull} />
+        <EmptyImg isNull={isNull} border={border} />
       )}
     </ContainerImg>
   );
@@ -28,6 +28,7 @@ ImageView.propTypes = {
   isNull: PropTypes.bool,
   imageUrl: PropTypes.string,
   height: PropTypes.string,
+  border: PropTypes.bool,
 };
 
 const ContainerImg = styled.div`
@@ -35,7 +36,6 @@ const ContainerImg = styled.div`
   width: ${(props) => props.width || "100%"};
   height: 0;
   padding-bottom: ${(props) => props.height || "calc(100% / 1.7)"};
-  display: flex;
 `;
 
 /*374px * 212px : 1.7*/
@@ -49,7 +49,7 @@ const EmptyBox = styled.div`
   background: #fafafa;
   border: 1px solid #e5e8ec;
   box-sizing: border-box;
-  border-radius: 5px;
+  border-radius: ${({ border }) => (border ? "10px" : "0px")};
   text-align: center;
 `;
 
@@ -57,7 +57,7 @@ const ImgArea = styled.img`
   position: absolute;
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  border-radius: ${({ border }) => (border ? "10px" : "0px")};
   background-repeat: no-repeat; /*background-image가 컨테이너를 가득 채우지 못할 경우에도 반복하지 않는다.*/
   background-size: cover; /*사이즈가 container에 맞지 않아도 꽉 차도록 채운다.*/
   background-position: center; /*background-image가 컨테이너에 가운데로 오도록 한다. */
