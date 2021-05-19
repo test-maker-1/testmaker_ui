0d38857287b83cedbc45322db9eb6ccf6dfb2da1
rktguswjd <010_cm@naver.com>
2021-05-19 16:57:31 +0900

    [feat] view/Feed -> 캐로셀, 태그, 테스트(카드), BottomBtn 컴포넌트 추가

--- src/view/Feed.js
+++ src/view/Feed.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import styled from "styled-components";
+import CarouselComponent from "../components/Feed/Carousel";
+import BottomBtn, { PageContainer } from "../components/frame/BottomBtn";
+import ENUM from "../constants/Enum";
+import TestsAtOneTag from "../components/Feed/TestsAtOneTag";
+const { PICKTEST } = ENUM;
+const Feed = (props) => {
+  return (
+    <Container>
+      <CarouselComponent />
+      <TestsAtOneTag />
+      <BottomBtn btnArr={[{ name: "테스트 만들기 도전!", type: PICKTEST }]} />
+    </Container>
+  );
+};
 
-const Feed = props => {
-  return <div>MAIN</div>
-}
+Feed.propTypes = {};
 
-Feed.propTypes = {}
+export default Feed;
 
-export default Feed
+const Container = styled(PageContainer)`
+  z-index: 0;
+`;
