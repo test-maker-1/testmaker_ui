63131c4093d85a5dda08ed675b3384c2ab0f67b0
rktguswjd <010_cm@naver.com>
2021-05-21 15:23:30 +0900

    [feat] Login 컴포넌트 1차 MVP 디자인 적용 및 수정

--- src/view/login/Login.jsx
+++ src/view/login/Login.jsx
@@ -1,23 +1,48 @@
 import React, { useCallback } from "react";
-import LoginBtn from "../../components/common/LoginBtn";
 import styled from "styled-components";
-import { KAKAO, OTHER } from "../../constants/Enum";
+import { KAKAO, EMAIL } from "../../constants/Enum";
 import usePage from "../../hooks/usePage";
+import { BtnField, SVG } from "../../components/common";
+import kakaoMd from "../../resources/images/kakaoMd.png";
 
 const Login = (props) => {
   const { goPage } = usePage();
-  const handleOnClick = useCallback(
-    (btnName, e) => {
-      if (btnName === KAKAO) goPage("/login/kakao");
-      else goPage("/login/other");
+  const kakao = () => {
+    return (
+      <>
+        <img src={kakaoMd} /> <div>{KAKAO}</div>{" "}
+      </>
+    );
+  };
+
+  const onClickKakao = useCallback(
+    (e) => {
+      goPage("/login/kakao");
     },
     [goPage]
   );
+
+  const onClickEmail = useCallback(
+    (e) => {
+      goPage("/login/email");
+    },
+    [goPage]
+  );
+
   return (
     <PageContainer>
-      <Logo />
-      <Name>TEST MAKER</Name>
-      <LoginBtn btns={[KAKAO, OTHER]} handleOnClick={handleOnClick} />
+      {/* <Logo>
+        <SVG type={ENUM.MAINLOGO} style={{ width: "200", height: "54" }} />
+      </Logo>
+      <Title>세컨드 모먼트</Title>
+      <SubTitle>30초 만에 테스트를 만들어보세요</SubTitle> */}
+      <BtnBox>
+        <BtnField name={KAKAO} color="kakao" onClick={onClickKakao} />
+      </BtnBox>
+
+      <BtnBox>
+        <BtnField name={EMAIL} color="skyBlue" onClick={onClickEmail} />
+      </BtnBox>
       <Summary>
         로그인 시 <b>개인정보보호정책</b>을 읽었으며 <br />
         <b>서비스 이용약관</b>에 동의하는 것으로 간주합니다.
@@ -29,24 +54,17 @@ const Login = (props) => {
 export default Login;
 
 export const PageContainer = styled.div`
-  padding: 0 20px;
+  padding: 0 ${({ theme: { paddings } }) => paddings.main}rem;
   width: 100%;
   text-align: center;
   margin: 0 auto;
 `;
 
 export const Logo = styled.div`
-  width: 107px;
-  height: 126px;
-  margin: 0 auto;
-  margin-top: 79px;
-  margin-bottom: 20px;
-  background: #fafafa;
-  border: 1px solid #e5e8ec;
-  text-align: center;
+  margin-top: 176px;
 `;
 
-export const Name = styled.div`
+export const Title = styled.div`
   margin: 0 auto;
   margin-bottom: 42px;
   font-style: normal;
@@ -56,17 +74,21 @@ export const Name = styled.div`
   color: #515966;
 `;
 
+export const BtnBox = styled.div`
+  padding: 0.5rem 0 0.5rem 0;
+`;
+
 export const Summary = styled.div`
   position: absolute;
   margin: 0 auto;
-  bottom: 40px;
+  bottom: 4rem;
   left: 0;
   right: 0;
-  font-size: 14px;
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.xs}rem;
   line-height: 21px;
-  color: #b7bdcb;
+  color: ${({ theme: { colors } }) => colors.gray};
   b {
     font-weight: bold;
-    color: #697382;
+    color: ${({ theme: { colors } }) => colors.blue};
   }
 `;
