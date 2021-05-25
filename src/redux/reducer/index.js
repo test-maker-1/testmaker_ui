1b4613e8ba8d3db4bc381593fb0727eff0221be6
rktguswjd <010_cm@naver.com>
2021-05-26 03:04:47 +0900

    [feat] reducer/index.js -> feed reducer 추가

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import making from "./makingReducer";
 import common from "./commonReducer";
 import testing from "./testingReducer";
+import feed from "./feedReducer";
 
 export const rootReducer = combineReducers({
   common: common.reducer,
   making: making.reducer,
   testing: testing.reducer,
+  feed: feed.reducer,
 });
