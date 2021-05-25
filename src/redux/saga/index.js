bdb6fe827fdb5433f368678c31a9ab2a018eea71
rktguswjd <010_cm@naver.com>
2021-05-26 03:05:11 +0900

    [feat] saga/index.js -> feed saga 추가

--- src/redux/saga/index.js
+++ src/redux/saga/index.js
@@ -1,10 +1,9 @@
-import { combineReducers } from 'redux';
-import { all } from 'redux-saga/effects';
-import userSaga from './userSaga';
-
-export const rootReducer = combineReducers({ userReducer })
+import { combineReducers } from "redux";
+import { all, fork } from "redux-saga/effects";
+import userSaga from "./userSaga";
+import feedSaga from "./feedSaga";
 
 export function* rootSaga() {
   //all 은 배열 안의 여러 사가를 동시에 실행
-  yield all([userSaga()]); 
+  yield all([fork(feedSaga)]);
 }
