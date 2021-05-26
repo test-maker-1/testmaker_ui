42e13be59f0859aa4f5ef67aa9647b8e97334da9
1000peach <_970813@daum.net>
2021-05-26 17:35:00 +0900

    [feat] making 관련 API 추가 -> 1차 MVP 필요

--- src/api/makingAPI.js
+++ src/api/makingAPI.js
@@ -1,7 +1,25 @@
-import { post } from "./instance";
+import { get, post } from "./instance";
 
 export default class MakingAPI {
+  // get
   static getTestId(params) {
     return post("/makingTest", params);
+  }
+  static getTest(testId) {
+    return get(`/makingTest/${testId}`);
+  }
+  // update or save
+  static updateOnFeed(params) {
+    const { testId, onFeed } = params;
+    const feedParams = {
+      onFeed,
+    };
+    return post(`/makingTest/feed/${testId}`, feedParams);
+  }
+  static saveTest(params) {
+    return post(`/makingTest/${params.testId}`, params);
+  }
+  static releaseTest(testId) {
+    return post(`/makingTest/submit/${testId}`);
   }
 }
