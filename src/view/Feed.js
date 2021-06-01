694bd5eaa11c2bb0954c3290414cfbc94d1e1c16
rktguswjd <010_cm@naver.com>
2021-06-01 22:59:51 +0900

    [feat] loding 추가

--- src/view/Feed.js
+++ src/view/Feed.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import CarouselComponent from "../components/Feed/Carousel";
@@ -8,6 +8,8 @@ import { initFeed, updateTests } from "../redux/reducer/feedReducer";
 import TagSwiper from "../components/common/TagSwiper";
 import Card from "../components/Feed/Card";
 import InfinScroll from "../components/common/InfinScroll";
+import { Loading } from "../components/common";
+import { StyledSpinner } from "../components/common/Loading";
 
 const { PICKTEST } = ENUM;
 const Feed = (props) => {
@@ -20,6 +22,7 @@ const Feed = (props) => {
     testsByTagError,
     testsByTag,
     lastTestUid,
+    changeTestsLoading,
   } = useSelector((state) => state.feed);
 
   useEffect(() => {
@@ -37,12 +40,14 @@ const Feed = (props) => {
 
   useEffect(() => {
     // 태그 바뀔 때 스크롤 상단으로
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, [selected]);
 
   return (
     <Container>
-      {feedLoading ? null : (
+      {feedLoading ? (
+        <Loading loading={feedLoading} />
+      ) : (
         <>
           <CarouselComponent />
           <>
@@ -55,24 +60,31 @@ const Feed = (props) => {
             </TagContainer>
 
             <CardContainer>
-              <InfinScroll
-                datas={testsByTag}
-                isStop={isStop}
-                getMoreDatas={getMoreDatas}
-              >
-                {testsByTag.map((test) => (
-                  <Card
-                    key={`test ${test.uid}`}
-                    title={test.title}
-                    coverImg={test.coverImg}
-                    makerName={test.maker.name}
-                    makerImg={test.makerImg}
-                    sharedCnt={test.sharedCnt}
-                    participatedCnt={test.participantsCnt}
-                    testLink={test.testLink}
-                  />
-                ))}
-              </InfinScroll>
+              {/* <Nothing /> */}
+              {changeTestsLoading ? (
+                <SpinnerContainer>
+                  <StyledSpinner />
+                </SpinnerContainer>
+              ) : (
+                <InfinScroll
+                  datas={testsByTag}
+                  isStop={isStop}
+                  getMoreDatas={getMoreDatas}
+                >
+                  {testsByTag.map((test) => (
+                    <Card
+                      key={`test ${test.uid}`}
+                      title={test.title}
+                      coverImg={test.coverImg}
+                      makerName={test.maker.name}
+                      makerImg={test.makerImg}
+                      sharedCnt={test.sharedCnt}
+                      participatedCnt={test.participantsCnt}
+                      testLink={test.testLink}
+                    />
+                  ))}
+                </InfinScroll>
+              )}
             </CardContainer>
           </>
           <BottomBtn
@@ -90,6 +102,9 @@ export default Feed;
 
 const Container = styled(PageContainer)`
   z-index: ${({ theme: { zIndex } }) => zIndex.feed};
+  .scroll-to-top {
+    opacity: 1;
+  }
 `;
 
 const TagContainer = styled.div`
@@ -100,4 +115,9 @@ const TagContainer = styled.div`
 
 const CardContainer = styled.div`
   margin-top: 2.8rem;
+`;
+
+const SpinnerContainer = styled.div`
+  text-align: center;
+  padding-top: 4rem;
 `;
