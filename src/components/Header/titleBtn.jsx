24bd15278984f24844744e86b9c4417a719b6ca4
1000peach <_970813@daum.net>
2021-06-02 01:25:18 +0900

    [feat] titleBtn css 수정
    - 이벤트 있는 element는 cursor: pointer
    - 이벤트 없는 element는 user-select: none

--- src/components/Header/titleBtn.jsx
+++ src/components/Header/titleBtn.jsx
@@ -1,51 +1,60 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+
 import { SVG } from "../common";
-import ENUM from "../../constants/Enum";
 import usePage from "../../hooks/usePage";
 
+import ENUM from "../../constants/Enum";
 import { LOGO, NOTHING, TITLE, CUSTOM } from "../../constants/headerInfo";
 
-/*
- * type: string;
- */
+// type: string;
 const TitleBtn = ({ type = LOGO, title = null }) => {
   const { goPage } = usePage();
   const { headTitle } = useSelector((state) => ({
     headTitle: state.common.headTitle,
   }));
+
   if (type === NOTHING) return null;
   if (type === TITLE) return <TitleText>{title}</TitleText>;
   if (type === CUSTOM) return <Step>{headTitle}</Step>;
 
-  const onClickEvent = () => {
+  const onClick = () => {
     if (type === LOGO) return goPage("/");
   };
 
-  return <Button onClick={onClickEvent}>{titleBtn[type]}</Button>;
-};
-const titleBtn = {
-  [LOGO]: <SVG type={ENUM.HEADERLOGO} style={{ width: "85", height: "24" }} />,
+  return titleBtn[type](onClick);
 };
 
-const Button = styled.button``;
+const titleBtn = {
+  [LOGO]: (onClick = null) => (
+    <SVG
+      type={ENUM.HEADERLOGO}
+      style={{ width: 85, height: 24 }}
+      onClick={onClick}
+    />
+  ),
+};
 
 const TitleText = styled.p`
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 24px;
   text-align: center;
+  user-select: none;
+
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.md}rem;
+  font-weight: bold;
+  line-height: 24px;
   letter-spacing: -0.5px;
-  color: #515966;
+  color: ${({ theme: { colors } }) => colors.bodyGray};
 `;
 
 const Step = styled.p`
-  font-size: 20px;
-  line-height: 30px;
   text-align: center;
+  user-select: none;
+
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.xl}rem;
+  line-height: 30px;
   letter-spacing: -0.8px;
-  color: #0b70fd;
+  color: ${({ theme: { colors } }) => colors.blue};
 `;
 
 export default TitleBtn;
