db9b18289a672491406ff7a39cd0c60d94deed02
rktguswjd <010_cm@naver.com>
2021-06-07 00:47:55 +0900

    [feat] MypageMain 로직 작성

--- src/view/mypage/MypageMain.jsx
+++ src/view/mypage/MypageMain.jsx
@@ -1,120 +1,139 @@
-import React, { memo, useState } from "react";
+import React, { memo, useEffect } from "react";
 import styled from "styled-components";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import { styled as mstyled } from "@material-ui/core/styles";
-import { TitleBox, ImageView } from "../../components/common";
+import { SVG } from "../../components/common";
+import ENUM from "../../constants/Enum";
+import Tab from "../../components/MyPage/Tab";
+import useCommon from "../../hooks/making/useCommon";
+import { partTests } from "../../redux/reducer/userReducer";
+
+import useUser from "../../hooks/useUser";
+import Error from "../Error";
+import TabTests from "../../components/MyPage/TabTests";
 
 const MypageMain = memo((props) => {
-  const [tab, setTab] = useState(0);
+  const { loggedIn, status, data, tabTests, selectedTab } = useUser();
+  const { dispatch } = useCommon();
 
-  return (
+  useEffect(() => {
+    if (!loggedIn) return <Error code={403} />;
+    dispatch(partTests({ num_elements: 20, uid: data.uid }));
+  }, []);
+
+  return status !== "success" ? null : (
     <div style={{ width: "100%" }}>
       <InfoContainer>
         <InfoUser>
-          <InfoAva>
-            <AvatarIcon alt="user" src="/static/images/avatar/1.jpg" />
-          </InfoAva>
-          <Partition>메이커짱짱</Partition>
+          <div className="space-left">
+            <InfoAva>
+              {data.profileImg ? (
+                <img src={data.profileImg} alt={"이미지"} style={imgStyle} />
+              ) : (
+                <EmptyImg />
+              )}
+            </InfoAva>
+            <Partition>{data.nickname}</Partition>
+          </div>
+          <div className="space-right">
+            <SVG
+              type={ENUM.STAR}
+              style={{
+                width: "24",
+                height: "24",
+              }}
+            />
+          </div>
         </InfoUser>
         <InfoBox>
           <InfoArea>
             <InfoSubBox>
               <InfoInBox>
                 <InfoTitle>북마크</InfoTitle>
-                <InfoCnt>15개</InfoCnt>
+                <InfoCnt>0개</InfoCnt>
               </InfoInBox>
             </InfoSubBox>
             <InfoSubBox>
               <InfoInBox>
                 <InfoTitle>내가 쓴 댓글</InfoTitle>
-                <InfoCnt>10</InfoCnt>
+                <InfoCnt>10개</InfoCnt>
               </InfoInBox>
             </InfoSubBox>
             <InfoSubBox>
               <InfoInBox>
                 <InfoTitle>참여 테스트</InfoTitle>
-                <InfoCnt>100</InfoCnt>
+                <InfoCnt>{data.didTestUids.length}개</InfoCnt>
               </InfoInBox>
             </InfoSubBox>
           </InfoArea>
         </InfoBox>
-        <BtnArea>
-          <BtnTab>내가 했던 테스트</BtnTab>
-          <BtnTab>내가 만든 테스트</BtnTab>
-          <BtnTab>임시저장</BtnTab>
-        </BtnArea>
+        <Tab />
       </InfoContainer>
-      {tab === 0 && (
-        <TitleBox title={"민지 우정테스트"}>
-          <ImageView />
-        </TitleBox>
-      )}
+      <TabTests />
     </div>
   );
 });
 
 const InfoContainer = styled.div`
-  padding: 16px 1.25em 0px; /*20px*/
+  padding: 16px 2rem 0px; /*20px*/
 `;
 
 const InfoUser = styled.div`
   margin-top: 7px;
+  display: flex;
+  justify-content: space-between;
+  .space-left,
+  .space-right {
+    align-items: center;
+    display: flex;
+  }
 `;
 
 const InfoAva = styled.div`
   display: inline-block;
+  width: 4rem;
+  height: 4rem;
   margin-right: 8px;
 `;
 
-const AvatarIcon = mstyled(Avatar)({
-  width: "40px",
-  height: "40px",
-  background: "#E5E8EC",
-  borderRadius: "20px",
-});
+const imgStyle = {
+  width: "100%",
+  height: "100%",
+  borderRadius: "50%",
+  margin: "0px auto",
+};
+
+const EmptyImg = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  margin: 0px auto;
+  background: #dadee6;
+`;
 
 const Partition = styled.p`
   display: inline-block;
-  font-size: ${({ theme: { fontSizes } }) => fontSizes.xs}rem; /*14px*/
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.md}rem; /*16px*/
   line-height: 21px;
   letter-spacing: -0.3px;
   font-style: normal;
   font-weight: bold;
-  font-size: 16px;
   line-height: 24px;
   letter-spacing: -0.5px;
   color: #515966;
-
-  &: last-child {
-    margin-left: 8px;
-  }
 `;
-
-const BtnArea = styled.div`
-  display: block;
-`;
-
-const BtnTab = mstyled(Button)({
-  border: "1px solid #E5E8EC",
-  boxSizing: "border-box",
-  borderRadius: "8px",
-  marginRight: "10px",
-});
 
 const InfoBox = styled.div`
   width: 100%; /*374px*/
   height: 83px;
-  background: #fafafa;
+  background: ${({ theme: { colors } }) => colors.ivory};
   border-radius: 13px;
-  margin: 1em 0px 1.25em;
+  margin: 1.2rem 0px 2.4rem;
+  color: ${({ theme: { colors } }) => colors.darkGray};
 `;
 
 const InfoArea = styled.div`
-display: flex;
-width: 100%;
-height: 100%;
-}
+  display: flex;
+  width: 100%;
+  height: 100%;
 `;
 
 const InfoSubBox = styled.div`
@@ -129,18 +148,16 @@ const InfoInBox = styled.div`
 `;
 
 const InfoTitle = styled.p`
-  font-size: 14px;
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.xs}rem; /*14px*/
   line-height: 21px;
   letter-spacing: -0.3px;
-  color: #363d4a;
 `;
 
 const InfoCnt = styled.p`
   font-weight: bold;
-  font-size: 20px;
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.xl}rem; /*20px*/
   line-height: 30px;
   letter-spacing: -0.8px;
-  color: #363d4a;
 `;
 
 export default MypageMain;
