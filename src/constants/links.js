dd439ee4670e03f92fd14bbe2b8137c8c62db8d2
1000peach <_970813@daum.net>
2021-06-18 15:14:36 +0900

    [feat] constants/links 추가 : 외부 링크 저장

---
+++ src/constants/links.js
@@ -0,0 +1,9 @@
+const login = {
+  termsOfService: "https://www.notion.so/16613e6cb495423f817ce6e683c3c5a2",
+  privacyPolicy: "https://www.notion.so/ae9b80575db64e14aa9882adc85a553e",
+};
+const links = {
+  login,
+};
+
+export default links;
