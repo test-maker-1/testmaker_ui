320e1c5aa4ea27771bd3118b9d7cd666143f1e1b
1000peach <_970813@daum.net>
2021-06-20 20:25:49 +0900

    [feat] 테스트 매이킹 LeftBtn onClick event custom
    - sequence step에 따라서 뒤로가기 기능 추가
    - step이 존재하지 않으면 일반 뒤로가기

--- src/components/Header/LeftBtn.jsx
+++ src/components/Header/LeftBtn.jsx
@@ -1,27 +1,45 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 import { NoticeAlert, SVG } from "../common";
 import usePage from "../../hooks/usePage";
 
-import { BACK, NOTHING, SEARCH } from "../../constants/headerInfo";
+import { BACK, MAKING_BACK, NOTHING, SEARCH } from "../../constants/headerInfo";
 import ENUM from "../../constants/Enum";
-/*
- * type: string;
- */
+import { seqTest } from "../../constants/urlInfo";
+
+// type: string;
 const LeftBtn = ({ type = BACK }) => {
-  const { goBack } = usePage();
+  const { goBack, goPage } = usePage();
+  const { pathname } = useLocation();
+
+  if (type === NOTHING) return null;
 
   const onClickEvent = () => {
     switch (type) {
       case BACK:
         return goBack();
+
+      case MAKING_BACK:
+        const [module, step] = [pathname.split("/")[2], pathname.split("/")[3]];
+        if (!module || !seqTest.hasOwnProperty(module)) return;
+
+        const sequence = seqTest[module];
+        if (!step || sequence.indexOf(step) < 0) return;
+
+        const prevIDX = sequence.indexOf(step) - 1;
+
+        if (prevIDX < 0) goBack();
+        else goPage(`/test/${module}/${sequence[prevIDX]}`);
+        return;
+
       case SEARCH:
         return NoticeAlert.open("곧 업데이트 예정이에요!");
+
       default:
         return null;
     }
   };
-
-  if (type === NOTHING) return null;
 
   return (
     <>
@@ -29,7 +47,7 @@ const LeftBtn = ({ type = BACK }) => {
         <NoticeAlert icon={ENUM.WARNING} btns={[{ name: "닫기" }]} />
       )}
       <SVG
-        type={type}
+        type={type === MAKING_BACK ? BACK : type}
         onClick={onClickEvent}
         style={{ width: "24", height: "24" }}
       />
