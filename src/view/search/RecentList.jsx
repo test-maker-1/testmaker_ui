e5e2cfcf2dd8dc1fd4dd820312119e7528394273
1000peach <codingandpeach@gmail.com>
2021-07-21 01:03:30 +0900

    [feat] RecentList 최근 검색어 리스트 ui 추가

--- src/view/search/RecentList.jsx
+++ src/view/search/RecentList.jsx
@@ -1,16 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 
+import { ReactComponent as Clock } from "../../resources/svg/clock.svg";
+import { ReactComponent as Cancel } from "../../resources/svg/cancel.svg";
+
 function RecentList() {
   return (
-    <ul>
-      <RecentItem>방탄 소년단 아미 테스트</RecentItem>
-    </ul>
+    <Container>
+      <RecentItem>
+        <TextWrap>
+          <Clock className="icon" />
+          <span className="text">방탄 소년단 아미 테스트</span>
+        </TextWrap>
+        <Cancel />
+      </RecentItem>
+    </Container>
   );
 }
 
+const Container = styled.ul`
+  li:first-child {
+    margin-top: 0 !important;
+  }
+`;
+
 const RecentItem = styled.li`
+  margin-top: 24px;
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TextWrap = styled.div`
+  display: flex;
+  align-items: center;
+
+  .icon {
+    margin-right: 8px;
+  }
+
+  .text {
+    width: 100%;
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.sm}rem;
+    line-height: 24px;
+    color: ${({ theme: { colors } }) => colors.darker};
+  }
 `;
 
 export default RecentList;
